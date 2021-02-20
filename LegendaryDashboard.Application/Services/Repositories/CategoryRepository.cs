8e430d76950b5b6e62d7281e6e2119a15a05dac2
FENNEC-LAPTOP\ffene <ffenek@outlook.com>
2021-02-20 18:07:04 +0300

    описать репозиторий категории

---
+++ LegendaryDashboard.Application/Services/Repositories/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using LegendaryDashboard.Domain.Models;
+using LegendaryDashboard.Infrastructure.DbContext;
+using LegendaryDashboard.Infrastructure.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace LegendaryDashboard.Application.Services.Repositories
+{
+    public sealed class CategoryRepository : Repository<Category, int>, ICategoryRepository
+    {
+        public CategoryRepository(DashboardContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Category>> GetByMatchingTitles(string approximateName,
+            CancellationToken cancellationToken)
+        {
+            return DbSet.Where(c => c.Title.Contains(approximateName)).ToList();
+        }
+
+        public async Task<Category> GetByTitles(string name, CancellationToken cancellationToken)
+        {
+            return await DbSet.FirstOrDefaultAsync(
+                c => c.Title == name, cancellationToken: cancellationToken);
+        }
+
+        public async Task<List<Category>> GetByParentCategoryId(int id, CancellationToken cancellationToken)
+        {
+            return DbSet.Where(c => c.ParentCategoryId == id).ToList();
+        }
+    }
+}
---
+++ LegendaryDashboard.Infrastructure/IRepositories/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using LegendaryDashboard.Domain.Models;
+
+namespace LegendaryDashboard.Infrastructure.IRepositories
+{
+    public interface ICategoryRepository : IRepository<Category, int>
+    {
+        
+        Task<List<Category>> GetByMatchingTitles(string approximateName, CancellationToken cancellationToken);
+        
+        Task<Category> GetByTitles(string name, CancellationToken cancellationToken);
+        
+        Task<List<Category>> GetByParentCategoryId(int id, CancellationToken cancellationToken);
+    }
+}
