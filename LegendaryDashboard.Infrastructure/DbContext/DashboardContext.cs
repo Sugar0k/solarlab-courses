b16f8547604d301bb66e7af3fda5a6c9e7682a28
alexb <alexbalashov201377@mail.ru>
2021-03-09 23:27:16 +0300

    Fix

--- LegendaryDashboard.Infrastructure/DbContext/DashboardContext.cs
+++ LegendaryDashboard.Infrastructure/DbContext/DashboardContext.cs
@@ -18,56 +18,17 @@ namespace LegendaryDashboard.Infrastructure.DbContext
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //Поменял @Stesniashka 
-            // Image:AdvertImage (O:O)
-            // modelBuilder.Entity<Image>()
-            //     .HasOne(i => i.AdvertImage)
-            //     .WithOne(ai => ai.Image);
-            
-            // Category:Category (M:O)
-            modelBuilder.Entity<Category>()
-                .HasMany(c => c.ChildCategories)
-                .WithOne(cc => cc.ParentCategory) 
-                .HasForeignKey(cc => cc.ParentCategoryId);
-
-            // Advert:AdvertImage (M:O)
-            // modelBuilder.Entity<Advert>()
-            //     .HasMany(a => a.AdvertImages)
-            //     .WithOne(ai => ai.Advert)
-            //     .HasForeignKey(ai => ai.AdvertId);
-            
             // Advert:UserAdvert (M:O)
             modelBuilder.Entity<Advert>()
                 .HasMany(a => a.UsersAdverts)
                 .WithOne(ua => ua.Advert)
                 .HasForeignKey(ua => ua.AdvertId);
-            //
-            // AdvertConnectionType:UserAdvert (M:O)
-            // modelBuilder.Entity<AdvertConnectionType>()
-            //     .HasMany(act => act.UsersAdverts)
-            //     .WithOne(ua => ua.Type)
-            //     .HasForeignKey(ua => ua.TypeId);
-            
+
             // UserAdvert:User (O:M)
             modelBuilder.Entity<UserAdvert>()
                 .HasOne(ua => ua.User)
                 .WithMany(u => u.UsersAdverts)
                 .HasForeignKey(ua => ua.UserId);
-
-            /*// User:Feedback (M:O)
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.TakenFeedbacks)
-                .WithOne(f => f.User)
-                .HasForeignKey(f => f.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
-            
-            // User:Feedback (M:O)
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.SentFeedbacks)
-                .WithOne(f => f.Commentator)
-                .HasForeignKey(f => f.CommentatorId)
-                .OnDelete(DeleteBehavior.Cascade);*/
-
         }
     }
 }
