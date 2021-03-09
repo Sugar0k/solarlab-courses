ad443e9a29a8c3480ec1be8a14de9de510180dcc
alexb <alexbalashov201377@mail.ru>
2021-03-09 23:24:04 +0300

    Fix

--- LegendaryDashboard.Infrastructure/DbContext/DashboardContext.cs
+++ LegendaryDashboard.Infrastructure/DbContext/DashboardContext.cs
@@ -18,7 +18,34 @@ namespace LegendaryDashboard.Infrastructure.DbContext
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-                       
+            //Поменял @Stesniashka 
+            // Image:AdvertImage (O:O)
+            modelBuilder.Entity<Image>()
+                .HasOne(i => i.AdvertImage)
+                .WithOne(ai => ai.Image);
+
+            // Image key field 
+            modelBuilder.Entity<Image>()
+                .HasKey(s => s.Guid);
+            
+            // Advert:AdvertImage (M:O)
+            modelBuilder.Entity<Advert>()
+                .HasMany(a => a.AdvertImages)
+                .WithOne(ai => ai.Advert)
+                .HasForeignKey(ai => ai.AdvertId);
+            
+            // Advert:UserAdvert (M:O)
+            modelBuilder.Entity<Advert>()
+                .HasMany(a => a.UsersAdverts)
+                .WithOne(ua => ua.Advert)
+                .HasForeignKey(ua => ua.AdvertId);
+            
+            // UserAdvert:User (O:M)
+            modelBuilder.Entity<UserAdvert>()
+                .HasOne(ua => ua.User)
+                .WithMany(u => u.UsersAdverts)
+                .HasForeignKey(ua => ua.UserId);
+            
         }
     }
 }
