2464798bf1e25cc4e7fc64603cd57f6360e7afd6
FENNEC-LAPTOP\ffene <ffenek@outlook.com>
2021-03-15 16:34:49 +0300

    убрать требование о статусе пользователя

--- LegendaryDashboard.Api/Controllers/Category/CategoryController.Create.cs
+++ LegendaryDashboard.Api/Controllers/Category/CategoryController.Create.cs
@@ -2,12 +2,15 @@
 using System.Threading.Tasks;
 using LegendaryDashboard.Application.Services.CategoryService.Interfaces;
 using LegendaryDashboard.Contracts.Contracts.Category.Requests;
+using LegendaryDashboard.Domain.Common;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LegendaryDashboard.Api.Controllers.Category
 {
     public partial class CategoryController
     {
+        //[Authorize(Roles = RoleConstants.AdminRole)]
         [HttpPost]
         [Route("create")]
         public async Task<IActionResult> Create(
