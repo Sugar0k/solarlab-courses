f1d1dad3e95cd9c013d160e7a3ffd102b32a1a2b
alexb <alexbalashov201377@mail.ru>
2021-04-03 22:41:29 +0300

    add modules in Startup.cs

---
+++ LegendaryDashboard.Application/ApplicationModule.cs
@@ -0,0 +1,64 @@
+using LegendaryDashboard.Application.Services.AdvertService.Implementations;
+using LegendaryDashboard.Application.Services.AdvertService.Interfaces;
+using LegendaryDashboard.Application.Services.CategoryService.Implementations;
+using LegendaryDashboard.Application.Services.CategoryService.Interfaces;
+using LegendaryDashboard.Application.Services.FeedbackService.Implementations;
+using LegendaryDashboard.Application.Services.FeedbackService.Interfaces;
+using LegendaryDashboard.Application.Services.FileService.Implementation;
+using LegendaryDashboard.Application.Services.FileService.Interfaces;
+using LegendaryDashboard.Application.Services.Repositories;
+using LegendaryDashboard.Application.Services.UserService.Implementations;
+using LegendaryDashboard.Application.Services.UserService.Interfaces;
+using LegendaryDashboard.Infrastructure.IRepositories;
+using LegendaryDashboard.Infrastructure.MapperProfiles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LegendaryDashboard.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplicationModule(this IServiceCollection services)
+        {
+            //добавление сервисов и репозиториев Категорий
+            services
+                .AddScoped<ICategoryService, CategoryService>()
+                .AddScoped<ICategoryRepository, CategoryRepository>()
+                
+                //подключение автомаппера
+                .AddAutoMapper(typeof(CategoryMapperProfile).Assembly);
+            
+            //добавление сервисов и репозиториев Пользователя
+            services
+                .AddScoped<IUserService, UserService>()
+                .AddScoped<IUserRepository, UserRepository>()
+                
+                //подключение автомаппера
+                .AddAutoMapper(typeof(UserMapperProfile).Assembly);
+
+            //добавление репозитория Объявления
+            services
+                .AddScoped<IAdvertRepository, AdvertRepository>();
+            
+            //добавление репозитория связи Пользователя и Объявления
+            services
+                .AddScoped<IUserAdvertRepository, UserAdvertRepository>();
+            
+            //добавление сервисов и репозиториев Отзыва
+            services
+                .AddScoped<IFeedbackService, FeedbackService>()
+                .AddScoped<IFeedbackRepository, FeedbackRepository>();
+            //добавление сервисов работы с файлами
+            services
+                .AddScoped<IFileService, FileService>();
+            //добавление репозитория картинок для объявления
+            services
+                .AddScoped<IAdvertImageRepository, AdvertImageRepository>();
+            //добавление сервиса Объявления
+            services
+                .AddScoped<IAdvertService, AdvertService>();
+
+            return services;
+        }
+        
+    }
+}
